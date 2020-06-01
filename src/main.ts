e4a094e177eef5317e2c22b4e57991cb84fa31a8
brad taniguchi <bradtaniguchi@gmail.com>
2020-05-31 18:29:16 -0700

    Add message for when server "starts"

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
     logger: false,
   });
   app.useLogger(logger);
-  await app.listen(3000);
+  await app.listen(3000, () => console.log('YOU WONT SEE ME'));
 }
 bootstrap();
